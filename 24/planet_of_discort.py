d083600a02c627416eabaa45b4740704bab3485d
lmaar <lukas.maar@student.tugraz.at>
2019-12-24 16:14:00 +0100

    [24] did default example

--- 24/planet_of_discort.py
+++ 24/planet_of_discort.py
@@ -62,9 +62,9 @@ def doPart1(map):
     printMap(map)
     while True:
         map = doStep(map)
-        # print("===================")
-        # print("after " + str(counter) + " minutes")
-        # printMap(map)
+        print("===================")
+        print("after " + str(counter) + " minutes")
+        printMap(map)
         value = getValue(map)
         if value not in list:
             list.append(value)
@@ -78,17 +78,80 @@ def doPart1(map):
 def getZeroMap(map):
     zero_map = []
     for y in range(len(map)):
-        zero_map.append(list(len(map[0]) * '.'))
+        zero_map.append(list(len(map) * '.'))
     zero_map[2][2] = '?'
     return zero_map
 
-def doStep2(maps, map):
+def changeBugs(maps, i):
+    map = maps[i][1]
+    next_map = copyMap(map)
+    for y in range(len(map)):
+        for x in range(len(map[0])):
+            if x == 2 and y == 2:
+                continue
+            count = 0
+            if i != 0:
+                neighbour_map = maps[i - 1][1]
+                if y == 0:
+                    count += int(neighbour_map[1][2] == "#")
+                elif y == len(map) - 1:
+                    count += int(neighbour_map[3][2] == "#")
+                if x == 0:
+                    count += int(neighbour_map[2][1] == "#")
+                elif x == len(map[0]) - 1:
+                    count += int(neighbour_map[2][3] == "#")
+            if i != len(maps) - 1:
+                neighbour_map = maps[i + 1][1]
+                if x == 2 and y == 1:
+                    count += int(neighbour_map[0][0] == "#")
+                    count += int(neighbour_map[0][1] == "#")
+                    count += int(neighbour_map[0][2] == "#")
+                    count += int(neighbour_map[0][3] == "#")
+                    count += int(neighbour_map[0][4] == "#")
+                elif x == 2 and y == 3:
+                    count += int(neighbour_map[4][0] == "#")
+                    count += int(neighbour_map[4][1] == "#")
+                    count += int(neighbour_map[4][2] == "#")
+                    count += int(neighbour_map[4][3] == "#")
+                    count += int(neighbour_map[4][4] == "#")
+                if y == 2 and x == 1:
+                    count += int(neighbour_map[0][0] == "#")
+                    count += int(neighbour_map[1][0] == "#")
+                    count += int(neighbour_map[2][0] == "#")
+                    count += int(neighbour_map[3][0] == "#")
+                    count += int(neighbour_map[4][0] == "#")
+                elif y == 2 and x == 3:
+                    count += int(neighbour_map[0][4] == "#")
+                    count += int(neighbour_map[1][4] == "#")
+                    count += int(neighbour_map[2][4] == "#")
+                    count += int(neighbour_map[3][4] == "#")
+                    count += int(neighbour_map[4][4] == "#")
+            count += countNeighbour(map, x, y)
+            print("i <" + str(i) + ">, depth <" + str(maps[i][0]) + ">, x <" + str(x) + ">, y <" + str(y) + ">, count <" + str(count) + ">")
+            if map[y][x] == '#' and count != 1:
+                next_map[y][x] = '.'
+            elif map[y][x] == '.' and (count == 1 or count == 2):
+                next_map[y][x] = '#'
+    printMap(map)
+    print("===========")
+    printMap(next_map)
+    return next_map
+
+def doStep2(maps):
     global counter
     if counter % 2 == 1:
-        count = (len(maps) + 1) / 2
-        maps.append([count, getZeroMap(map)])
-        count *= -1
-        maps.append([count, getZeroMap(map)])
+        depth = (len(maps) + 1) / 2
+        maps.append([int(depth), getZeroMap(maps[0][1])])
+        depth *= -1
+        maps.append([int(depth), getZeroMap(maps[0][1])])
+        maps = sorted(maps, key=lambda x: x[0], reverse=False)
+    next_maps = []
+    for i in range(len(maps)):
+        next_maps.append([maps[i][0], changeBugs(maps, i)])
+    return next_maps
+    # for i in range(len(maps)):
+    #     maps[i][1] = changeBugs(maps, i)
+    # return maps
 
 def printMaps(maps):
     maps = sorted(maps, key=lambda x: x[0], reverse=False)
@@ -105,12 +168,22 @@ def doPart2(map):
     print("init")
     printMap(map)
     counter += 1
-    for i in range(3):
+    for i in range(10):
         print("===================")
         print("after " + str(counter) + " minutes")
-        doStep2(maps, map)
+        maps = doStep2(maps)
         printMaps(maps)
         counter += 1
+    print("count of bugs is " + str(countBugs(maps)))
+
+def countBugs(maps):
+    count = 0
+    for map in maps:
+        for line in map[1]:
+            for c in line:
+                if c == '#':
+                    count += 1
+    return count
 
 def main():
     map = readInput()
